a88d0103860a3c6543eaf365b7a00c787e605a38
brandireger <brandi.reger@gmail.com>
2020-02-13 12:01:01 -0600

    updated with class examples

--- 3.8.3_subqueries_exercises.sql
+++ 3.8.3_subqueries_exercises.sql
@@ -2,7 +2,7 @@ SELECT first_name, last_name
 FROM employees
 WHERE last_name IN ('Vidya', 'Maya', 'Irena')
 
--- SAMPLE
+-- SAMPLES
 SELECT column_a, column_b, column_c
 FROM table_a
 WHERE column_a IN (
@@ -38,67 +38,54 @@ GROUP BY title;
 SELECT CONCAT(first_name, ' ', last_name) AS 'Employee Name', hire_date
 FROM employees
 WHERE hire_date IN (
-	SELECT hire_date
-	FROM employees
-	WHERE emp_no = 101010);
+		SELECT hire_date FROM employees
+		WHERE emp_no = 101010);
 
 -- 2. Find all the titles held by all employees with the first name Aamod.
 -- Results in 314 total titles
-SELECT title
-FROM titles
+SELECT title FROM titles
 WHERE emp_no IN (
-	SELECT emp_no
-	FROM employees
-	WHERE first_name = 'Aamod');
+		SELECT emp_no FROM employees
+		WHERE first_name = 'Aamod');
 
 -- Results in 6 unique titles
-SELECT title, count(*)
-FROM titles
+SELECT title, count(*) FROM titles 	-- SELECT DISTINCT title
 WHERE emp_no IN (
-	SELECT emp_no
-	FROM employees
-	WHERE first_name = 'Aamod')
+		SELECT emp_no FROM employees
+		WHERE first_name = 'Aamod')
 GROUP BY title;
 
 -- 3. How many people in the employees table are no longer working for the company?
-SELECT COUNT(emp_no)
-FROM employees
-WHERE emp_no IN (
-	SELECT emp_no
-	FROM dept_emp
-	WHERE to_date < NOW());
+SELECT COUNT(emp_no) FROM employees
+WHERE emp_no NOT IN (
+		SELECT emp_no FROM dept_emp
+		WHERE to_date < NOW());
 
 -- 4. Find all the current department managers that are female.
-SELECT first_name, last_name
-FROM employees
+SELECT first_name, last_name FROM employees
 WHERE emp_no IN (
-	SELECT emp_no
-	FROM dept_manager
-	WHERE to_date > NOW())
+		SELECT emp_no FROM dept_manager
+		WHERE to_date > NOW())
 AND gender = 'F';
 
 -- 5. Find all the employees that currently have a higher than average salary.
-SELECT first_name, last_name, salary
-FROM employees
+SELECT first_name, last_name, salary FROM employees
 JOIN salaries USING(emp_no)
 WHERE emp_no IN (
-	SELECT emp_no
-	FROM salaries
-	WHERE salary > (SELECT AVG(salary) FROM salaries)
-	AND to_date > NOW())
-	AND salaries.to_date > NOW()
+		SELECT emp_no FROM salaries
+		WHERE salary > (SELECT AVG(salary) FROM salaries)
+						AND to_date > NOW())
+		AND salaries.to_date > NOW()
 ;
 
 -- 6. How many current salaries are within 1 standard deviation of the highest salary?
-SELECT *
-FROM salaries
-WHERE salary > (SELECT MAX(salary) - STD(salary)
-	FROM salaries)
-	AND to_date > NOW();
+SELECT * FROM salaries
+WHERE salary > 
+		(SELECT MAX(salary) - STD(salary) FROM salaries)
+AND to_date > NOW();
 
 -- BONUS 1. Find all the department names that currently have female managers.
-SELECT dept_name
-FROM departments
+SELECT dept_name FROM departments
 RIGHT JOIN (
 	SELECT dept_no FROM dept_manager
 	WHERE emp_no IN 
@@ -108,7 +95,16 @@ RIGHT JOIN (
 ORDER BY dept_name;
 
 -- Bonus 2. Find the first and last name of the employee with the highest salary.
+SELECT first_name, last_name FROM employees
+WHERE emp_no = (SELECT emp_no FROM salaries
+				WHERE salary = 
+						(SELECT MAX(salary) FROM salaries
+						WHERE to_date > NOW()));
 
 -- Bonus 3. Find the department name that the employee with the highest salary works in.
-
+SELECT dept_name FROM employees_with_departments
+WHERE emp_no = (SELECT emp_no FROM salaries
+				WHERE salary = 
+							(SELECT MAX(salary) FROM salaries
+							WHERE to_date > NOW()));
 
